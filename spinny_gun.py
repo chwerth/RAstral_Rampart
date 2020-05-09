6cad11d966ef89e76c6e5a66fe3bc6bdfd618bae
Caleb Werth <cwerth@crimson.ua.edu>
2020-05-09 10:56:36 -0500

    Menu Implementation (#21)
    * We have a menu boys
    
    * Black formatting
    
    * text tweak
    
    * Missing docstring

--- spinny_gun.py
+++ spinny_gun.py
@@ -8,8 +8,12 @@ DISPLAY_WIDTH = 800
 DISPLAY_HEIGHT = 800
 
 # Colors
+BLACK = (0, 0, 0)
 WHITE = (255, 255, 255)
 GOLD = (218, 165, 32)
+GREEN = (0, 255, 0)
+RED = (255, 0, 0)
+LIGHT_YELLOW = (100, 100, 93)
 
 # Initializing
 pygame.init()
@@ -17,22 +21,57 @@ SCREEN = pygame.display.set_mode((DISPLAY_WIDTH, DISPLAY_HEIGHT))
 pygame.display.set_caption("Spinny Gun")
 CLOCK = pygame.time.Clock()
 
+# Text
+GIANT_TEXT = pygame.font.Font("freesansbold.ttf", 115)
+MEDIUM_TEXT = pygame.font.Font("freesansbold.ttf", 30)
+SMALL_TEXT = pygame.font.Font("freesansbold.ttf", 20)
+
+
+def exit_game():
+    """Exits the the game"""
+    pygame.quit()
+    quit()
+
 
 def rot_center(image, rect, angle):
-    """rotate an image while keeping its center"""
+    """Rotate an image while keeping its center"""
     rot_image = pygame.transform.rotate(image, angle)
     rot_rect = rot_image.get_rect(center=rect.center)
     return rot_image, rot_rect
 
 
+def text_objects(text, font, color, pos):
+    """Return text surface and rect"""
+    text_surface = font.render(text, True, color)
+    text_rect = text_surface.get_rect(center=pos)
+    return text_surface, text_rect
+
+
+def intersects(rect, radius, center):
+    """Test if rect intersects with circle"""
+    circle_distance_x = abs(center[0] - rect.centerx)
+    circle_distance_y = abs(center[1] - rect.centery)
+    if (
+        circle_distance_x > rect.w / 2.0 + radius
+        or circle_distance_y > rect.h / 2.0 + radius
+    ):
+        return False
+    if circle_distance_x <= rect.w / 2.0 or circle_distance_y <= rect.h / 2.0:
+        return True
+    corner_x = circle_distance_x - rect.w / 2.0
+    corner_y = circle_distance_y - rect.h / 2.0
+    corner_distance_sq = corner_x ** 2.0 + corner_y ** 2.0
+    return corner_distance_sq <= radius ** 2.0
+
+
 class SpinnyGun(object):
     """The rotating gun that the player can fire"""
 
-    def __init__(self, display, position):
+    def __init__(self, display, pos):
         self.display = display
         self.image = pygame.image.load("assets/gun.png")
         self.rotated_image = self.image
-        self.rect = self.image.get_rect(center=position)
+        self.rect = self.image.get_rect(center=pos)
         self.angle = 0
         self.turning_left = True
 
@@ -63,17 +102,17 @@ class SpinnyGun(object):
 class Projectile(object):
     """This is what the Spinny Gun fires"""
 
-    def __init__(self, display, position, angle):
+    def __init__(self, display, pos, angle):
         self.display = display
-        self.x_pos = position[0]
-        self.y_pos = position[1]
+        self.x_pos = pos[0]
+        self.y_pos = pos[1]
         self.speed = 5
         self.x_vel = -round(self.speed * sin(radians(angle)))
         self.y_vel = -round(self.speed * cos(radians(angle)))
         self.radius = 8
 
     def draw(self):
-        """Draws circle on display at x and y position"""
+        """Draws circle on display at x and y pos"""
         pygame.draw.circle(
             self.display, GOLD, (self.x_pos, self.y_pos), self.radius
         )
@@ -82,6 +121,21 @@ class Projectile(object):
         """Moves according to x and y velocity"""
         self.x_pos += self.x_vel
         self.y_pos += self.y_vel
+
+    def update(self, projectiles):
+        """
+        Update position of projectile
+        and check it it's off-screen
+        """
+        if (
+            self.x_pos > DISPLAY_WIDTH
+            or self.x_pos < 0
+            or self.y_pos > DISPLAY_HEIGHT
+            or self.y_pos < 0
+        ):
+            projectiles.pop(projectiles.index(self))
+        self.move()
+        self.draw()
 
 
 class Missile(object):
@@ -96,26 +150,158 @@ class Missile(object):
         self.y_pos = -600
 
     def blit(self):
-        """Draws self at current position"""
+        """Draws self at current pos"""
         self.display.blit(self.rotated_image, (self.x_pos, self.y_pos))
 
     def move(self):
-        """Updates y position to move down"""
+        """Updates y pos to move down"""
         self.y_pos += self.speed
+
+
+class Button(object):
+    """Generic button with text"""
+
+    def __init__(self, text, rect_dim, color, function):
+        self.rect = pygame.Rect(rect_dim)
+        self.text = text
+        self.text_rect = self.text.get_rect(center=self.rect.center)
+        self.color = color
+        self.function = function
+
+    def draw(self):
+        """Draw button with text"""
+        pygame.draw.rect(SCREEN, self.color, self.rect)
+        SCREEN.blit(self.text, self.text_rect)
+
+    def hit(self):
+        """What the button does when hit"""
+        self.function()
+
+
+def about_page():
+    """The about page of Spinny Gun"""
+
+    credit_surf_1, credit_rect_1 = text_objects(
+        "Spinny Gun was created by Caleb Werth,",
+        MEDIUM_TEXT,
+        BLACK,
+        (DISPLAY_WIDTH * 0.5, DISPLAY_HEIGHT * 0.375),
+    )
+    credit_surf_2, credit_rect_2 = text_objects(
+        "Russell Spry, and Aaron Werth",
+        MEDIUM_TEXT,
+        BLACK,
+        (DISPLAY_WIDTH * 0.5, DISPLAY_HEIGHT * 0.4375),
+    )
+    instructions_surf, instructions_rect = text_objects(
+        "Press space to return to menu",
+        MEDIUM_TEXT,
+        BLACK,
+        (DISPLAY_WIDTH * 0.5, DISPLAY_HEIGHT * 0.65),
+    )
+
+    while True:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                exit_game()
+
+            if event.type == pygame.KEYUP:
+                if event.key == pygame.K_SPACE:
+                    game_menu()
+
+        SCREEN.fill(WHITE)
+        SCREEN.blit(credit_surf_1, credit_rect_1)
+        SCREEN.blit(credit_surf_2, credit_rect_2)
+        SCREEN.blit(instructions_surf, instructions_rect)
+
+        pygame.display.update()
+        CLOCK.tick(60)
+
+
+def game_menu():
+    """The menu for the game"""
+
+    gun = SpinnyGun(SCREEN, (DISPLAY_WIDTH * 0.5, DISPLAY_HEIGHT * 0.875))
+    projectiles = []
+    start_button = Button(
+        SMALL_TEXT.render("Start", True, BLACK),
+        ((DISPLAY_WIDTH * 0.16), (DISPLAY_HEIGHT * 0.65), 100, 50),
+        GREEN,
+        game_loop,
+    )
+    about_button = Button(
+        SMALL_TEXT.render("About", True, BLACK),
+        ((DISPLAY_WIDTH * 0.43), (DISPLAY_HEIGHT * 0.5), 100, 50),
+        LIGHT_YELLOW,
+        about_page,
+    )
+    quit_button = Button(
+        SMALL_TEXT.render("Quit", True, BLACK),
+        ((DISPLAY_WIDTH * 0.70), (DISPLAY_HEIGHT * 0.65), 100, 50),
+        RED,
+        exit_game,
+    )
+    buttons = [start_button, about_button, quit_button]
+
+    while True:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                exit_game()
+
+            if event.type == pygame.KEYUP:
+                if event.key == pygame.K_SPACE:
+                    projectiles.append(
+                        Projectile(SCREEN, gun.rect.center, gun.angle)
+                    )
+
+        SCREEN.fill(WHITE)
+        text_surf_title, text_rect_title = text_objects(
+            "Spinny Gun",
+            GIANT_TEXT,
+            BLACK,
+            ((DISPLAY_WIDTH * 0.5), (DISPLAY_HEIGHT * 0.2)),
+        )
+        SCREEN.blit(text_surf_title, text_rect_title)
+
+        text_surf_space, text_rect_space = text_objects(
+            "Press Space To Shoot!",
+            MEDIUM_TEXT,
+            BLACK,
+            ((DISPLAY_WIDTH * 0.5), (DISPLAY_HEIGHT * 0.32)),
+        )
+        SCREEN.blit(text_surf_space, text_rect_space)
+
+        gun.rotate()
+        gun.blit()
+
+        for button in buttons:
+            button.draw()
+
+        for projectile in projectiles:
+            projectile.update(projectiles)
+            for button in buttons:
+                if intersects(
+                    button.rect,
+                    projectile.radius,
+                    (projectile.x_pos, projectile.y_pos),
+                ):
+                    button.hit()
+
+        pygame.display.update()
+        CLOCK.tick(60)
 
 
 def game_loop():
     """The main game loop"""
 
-    game_exit = False
     gun = SpinnyGun(SCREEN, (DISPLAY_WIDTH * 0.5, DISPLAY_HEIGHT * 0.875))
     missile = Missile(SCREEN)
     projectiles = []
 
-    while not game_exit:
+    while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
-                game_exit = True
+                exit_game()
 
             # Fire a projectile if the player presses and releases space
             if event.type == pygame.KEYUP:
@@ -129,15 +315,7 @@ def game_loop():
 
         # If a projectile moves off-screen, remove it from the list
         for projectile in projectiles:
-            if (
-                projectile.x_pos > DISPLAY_WIDTH
-                or projectile.x_pos < 0
-                or projectile.y_pos > DISPLAY_HEIGHT
-                or projectile.y_pos < 0
-            ):
-                projectiles.pop(projectiles.index(projectile))
-            projectile.move()
-            projectile.draw()
+            projectile.update(projectiles)
 
         # Rotate and draw gun
         gun.rotate()
@@ -156,6 +334,5 @@ def game_loop():
         CLOCK.tick(60)
 
 
-game_loop()
-pygame.quit()
-quit()
+if __name__ == "__main__":
+    game_menu()
