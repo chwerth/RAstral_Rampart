b1b2df12663dff4cf5ac196d47db3db5e51a6dc7
Caleb Werth <cwerth@crimson.ua.edu>
2020-05-31 11:51:21 -0500

    Version bump

--- setup.py
+++ setup.py
@@ -12,7 +12,7 @@ if sys.platform == "win32":
 
 setup(
     name="RAstral Rampart",
-    version="2.1.0",
+    version="2.2.0",
     description="test",
     author="Caleb Werth & Russell Spry",
     options={
